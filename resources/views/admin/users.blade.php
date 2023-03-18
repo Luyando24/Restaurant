e220d4d34d19dfeea17d9907fa3ea472254860b6
Luyando24 <cluyando2@gmail.com>
2023-03-18 12:04:38 +0800

    first commit

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Corona Admin</title>
+    <!-- plugins:css -->
+    @include('admin.admincss');
+  </head>
+  <body>
+  <x-app-layout>
+ 
+ </x-app-layout>
+ <div class="container-scroller">
+ @include('admin.navbar');
+     <h1>TABLE</h1>
+   
+</div>
+   @include('admin.adminscript');
+  </body>
+</html>
